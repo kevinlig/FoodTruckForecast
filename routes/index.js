6879b776079526010d02971a2cb8d50eb4fed636
kevinlig <kevinlig@gmail.com>
2014-05-12 19:49:16 -0400

    adding google analytics

--- routes/index.js
+++ routes/index.js
@@ -16,4 +16,36 @@ router.get('/js/chartData.json', function(req, res) {
 	forecast.displayForecastGraph(res);
 });
 
+router.get('/js/googleAnalytics.js', function(req, res) {
+
+	var googleUA;
+	var googleUrl;
+
+	if (process.env.PROD_ENV === undefined) {
+		var localVars = require('../localvars');
+		googleUA = localVars.googleUA;
+		googleUrl = localVars.googleUrl;
+	}
+	else {
+		googleUA = process.env.GOOGLE_UA;
+		googleUrl = process.env.GOOGLE_URL;
+	}
+
+
+	var scriptString = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),";
+	scriptString += "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', '";
+
+	scriptString += googleUA;
+
+	scriptString += "', '";
+
+	scriptString += googleUrl;
+
+	scriptString += "');ga('send', 'pageview');";
+
+	res.set('Content-Type','application/javascript');
+	res.send(scriptString);
+
+});
+
 module.exports = router;
